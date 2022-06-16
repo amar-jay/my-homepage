2fca40057b38390c091b43beae97ecbf42fc337e
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-06-16 18:07:48 +0300

    NavBar designed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import Layout from "../components/layout/main";
+import Layout from "../components/layout/Main";
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
