cc0c9e1b4df29d4d51ca0f315233b20a260ffcc6
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-06-16 19:43:32 +0300

    Still trying on the colorMode issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,24 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  ColorModeScript,
+  extendTheme,
+  type ThemeConfig,
+} from "@chakra-ui/react";
 import Layout from "../components/layout/Main";
 
+const config: ThemeConfig = {
+  initialColorMode: "light",
+  useSystemColorMode: false,
+};
+
+// 3. extend the theme
+const theme = extendTheme({ config });
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <ChakraProvider>
+      <ColorModeScript initialColorMode="light" />
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
       </Layout>
