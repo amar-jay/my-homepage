88faeae1a86e7553f408f3ac0b824f7eb2731b19
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-07-02 11:30:48 +0300

    fetchImages has dependency errors-must fix

--- components/layout/Carousel.tsx
+++ components/layout/Carousel.tsx
@@ -1,23 +1,23 @@
 import { Box, Container, Heading, Image } from "@chakra-ui/react";
 import Carousel from "framer-motion-carousel";
-import NextImage from "next/image";
+// import NextImage from "next/image";
 import fs from "fs/promises";
 import path from "path";
 import { cwd } from "process";
 import { useCallback, useEffect, useState } from "react";
-const fetchImages = async () => {
-  const files = await fs
-    .readdir(path.join(cwd(), "/public/images/carousel"), "utf-8")
-    .catch((err) => console.error(err));
-  return files!;
-};
+// const fetchImages = async () => {
+//   const files = await fs
+//     .readdir(path.join(cwd(), "/public/images/carousel"), "utf-8")
+//     .catch((err) => console.error(err));
+//   return files!;
+// };
 const App: React.FC<{ imageUrl: string[] }> = ({ imageUrl }) => {
-  const [images, setImages] = useState<string[]>([]);
+  // const [images, setImages] = useState<string[]>([]);
 
-  useCallback(async () => {
-    const images = await fetchImages();
-    setImages(images);
-  }, [fetchImages]);
+  // useCallback(async () => {
+  //   const images = await fetchImages();
+  //   setImages(images);
+  // }, [fetchImages]);
   return (
     <>
       <Container paddingTop={3}>
