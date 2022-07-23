38f0da4475e73674ce399e0ea0c5861359fff5f0
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-07-23 23:30:40 +0300

    Revert "sanity studio configured"
    This reverts commit 7a4b8e4a6d0651c0b282a1c82ded56d0ca1bbfb8.

--- db/blog/schemas/author.js
+++ db/blog/schemas/author.js
@@ -25,6 +25,19 @@ export default {
         hotspot: true,
       },
     },
+    {
+      name: 'bio',
+      title: 'Bio',
+      type: 'array',
+      of: [
+        {
+          title: 'Block',
+          type: 'block',
+          styles: [{title: 'Normal', value: 'normal'}],
+          lists: [],
+        },
+      ],
+    },
   ],
   preview: {
     select: {
