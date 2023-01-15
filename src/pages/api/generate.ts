5c4deaa92276315f83659fe6ab889dbdaaacc0f2
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2023-01-15 14:34:12 +0300

    open ai api key

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -10,12 +10,16 @@ export default async function openaihandler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
+  try {
   const completion = await openai.createCompletion({
     model: "text-davinci-002",
     prompt: generatePrompt(req.body.input),
     temperature: 0.6,
   });
   res.status(200).json({ result: completion.data.choices });
+  } catch (err) {
+    res.status(404).json({error: err});
+  }
 }
 
 function generatePrompt<T extends string>(prompt: T): T {
